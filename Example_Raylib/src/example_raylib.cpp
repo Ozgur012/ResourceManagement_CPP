2f80066d2972568fca2e6e87372d4ead57d78c5d
OzgurDESKTOP <ozgurkalay2017@gmail.com>
2025-06-30 14:01:55 +0100

    example raylib updated

--- Example_Raylib/src/example_raylib.cpp
+++ Example_Raylib/src/example_raylib.cpp
@@ -13,25 +13,30 @@ int main()
     SetTargetFPS(60);
 
 
-    ResourceManagement::ResourceLoader::init("resources.pk");
+    ResourceManagement::ResourceLoader::init("resources.pk"); // A path is supplied here. It is relative to your binary (exe).
+
+    // Loading resources
+
+    // Access name example:
+    //      Input dir: c:/Projects/MyAwsomegame/Assets
+    //      Packed file: c:/Projects/MyAwsomegame/Assets/Textures/Player/player.png
+    //      Access name: Assets/Textures/Player/player.png
 
     // Texture
-    std::vector<char> img_data = ResourceManagement::ResourceLoader::get_raw_resource_data("Assets/Textures/Icons/ammo_01.PNG");
+    std::vector<char> img_data = ResourceManagement::ResourceLoader::get_raw_resource_data("access name of your png");
     Image img = LoadImageFromMemory(".png", (unsigned char*)img_data.data(), img_data.size());
     Texture2D texture = LoadTextureFromImage(img);
     UnloadImage(img);
 
     // Music
-    std::vector<char> music_data = ResourceManagement::ResourceLoader::get_raw_resource_data("Assets/Audio/Music/Triumphant Orchestral Theme #4.wav");
+    std::vector<char> music_data = ResourceManagement::ResourceLoader::get_raw_resource_data("access name of your wav");
     Music music = LoadMusicStreamFromMemory(".wav", (unsigned char*)music_data.data(), music_data.size());
     PlayMusicStream(music);
     
     // Sound
-    std::vector<char> sound_data = ResourceManagement::ResourceLoader::get_raw_resource_data("Assets/Audio/Soundfx/chimes_magic_bell_ding_5.wav");
+    std::vector<char> sound_data = ResourceManagement::ResourceLoader::get_raw_resource_data("access name of your wav");
     Wave s_wave = LoadWaveFromMemory(".wav", (unsigned char*)sound_data.data(), sound_data.size());
     Sound sound = LoadSoundFromWave(s_wave);
-
-    std::cout << "Checking if defs\n";
 
     while (!WindowShouldClose())
     {
